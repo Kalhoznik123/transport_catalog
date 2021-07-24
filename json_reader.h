8b5eeb1155ef5f2c486526ce595e6544a4dc00a5
Kalhoznik123 <dr.kalhoznik@yandex.ru>
2021-07-25 02:19:26 +0300

    fix

--- json_reader.h
+++ json_reader.h
@@ -10,40 +10,40 @@ namespace transport {
 
 class Reader {
 public:
-    explicit Reader(transport::Catalogue& catalogue, renderer::MapRenderer& renderer);
+  explicit Reader(transport::Catalogue& catalogue, renderer::MapRenderer& renderer);
 
-    // парсит текст из потоков в JSON
-    void FillCatalog(std::istream& in);
+  // парсит текст из потоков в JSON
+  void FillCatalog(std::istream& in);
 
-    void PrintRequests(std::ostream& out) const;
+  void PrintRequests(std::ostream& out) const;
 
   static  svg::Color ParseColor(json::Node node) ;
 private:
 
-    renderer::RenderSettings ParseVisualisationSettings() const;
-    // добавляет в базу остановки
-    void AddStops() const;
+  renderer::RenderSettings ParseVisualisationSettings() const;
+  // добавляет в базу остановки
+  void AddStops() const;
 
-    // добавляет в базу расстояния между остановками
-    void AddDistances() const;
+  // добавляет в базу расстояния между остановками
+  void AddDistances() const;
 
-    // добавляет в базу маршруты
-    void AddBuses() const;
+  // добавляет в базу маршруты
+  void AddBuses() const;
 
-    json::Node StopStat(const json::Node& node) const;
+  json::Node StopStat(const json::Node& node) const;
 
-    json::Node RouteStat(const json::Node& node) const;
+  json::Node RouteStat(const json::Node& node) const;
 
-    json::Node MapStat(const json::Node& node) const;
+  json::Node MapStat(const json::Node& node) const;
 
-    json::Document returnStat() const;
+  json::Document returnStat() const;
 
 
-    transport::Catalogue& catalogue_;
-    renderer::MapRenderer& renderer_;
-    json::Array base_requests_;
-    json::Array stat_requests_;
-    json::Dict render_settings_;
+  transport::Catalogue& catalogue_;
+  renderer::MapRenderer& renderer_;
+  json::Array base_requests_;
+  json::Array stat_requests_;
+  json::Dict render_settings_;
 };
 
 } // namespace json
